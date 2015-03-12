d3719521a695c9a4a9b9f91a346c6628c86b7905
yjkim <yjkim@yellomobile.com>
2015-03-12 18:25:43 +0900

    cookie set 도메인설정, httponly 적용

--- request/cookie.php
+++ request/cookie.php
@@ -5,10 +5,12 @@ use limepie\request;
 class cookie extends request
 {
 
-    public static function set($key, $value)
+    public static function set($key, $value, $expire=0)
     {
 
-        return parent::$data['cookie'][$key] = $_COOKIE[$key] = $value;
+        $cdomain = defined('CDOMAIN')?CDOMAIN:FALSE;
+        setcookie($key,$value,$expire,'/',$cdomain,FALSE,TRUE);
+        return parent::$data['cookie'][$key] = $value;
 
     }
 
