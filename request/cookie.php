7c81a40de3a056467e1b0aa898ee3a6e69f0ec58
yejune <smkwon@yellomobile.com>
2015-04-08 18:00:41 +0900

    arguments에 path, domain, secure, httponly 추가

--- request/cookie.php
+++ request/cookie.php
@@ -5,9 +5,10 @@ use limepie\request;
 class cookie extends request
 {
 
-    public static function set($key, $value)
+    public static function set($key, $value, $expire = 0, $path = NULL, $domain = NULL, $secure = FALSE, $httponly = FALSE)
     {
 
+        setcookie($key, $value, $expire, $path, $domain, $secure, $httponly);
         return parent::$data['cookie'][$key] = $_COOKIE[$key] = $value;
 
     }
